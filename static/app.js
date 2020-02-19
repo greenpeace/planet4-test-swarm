b6ed700d653d6f2b15169764bf1d5603ad5228ae
Nikos Roussos <nikos@roussos.cc>
2020-02-19 09:36:00 +0200

    Add CI link

--- static/app.js
+++ static/app.js
@@ -17,17 +17,19 @@ request.onload = function() {
       const title = item[0];
       const git_url = 'https://github.com/greenpeace/planet4-test-' + title;
       const web_url = 'https://k8s.p4.greenpeace.org/test-' + title;
+      const ci_url = 'https://circleci.com/gh/greenpeace/workflows/planet4-test-' + title;
       if (item[1] === 1) {
         status = 'on';
       }
       html += `<div class="item">
         <div class="title ${status}">${title}</div>
-        <a href="${git_url}" target="_blank" class="link"><img src="static/git-square.svg" alt="source"></a>
-        <a href="${web_url}" target="_blank" class="link"><img src="static/external-link-square.svg" alt="web"></a>`
+        <a href="${git_url}" target="_blank" class="link"><img src="static/git-square.svg" alt="source" title="source code"></a>
+        <a href="${web_url}" target="_blank" class="link"><img src="static/external-link-square.svg" alt="web" title="website"></a>
+        <a href="${ci_url}" target="_blank" class="link"><img src="static/circle-o.svg" alt="ci" title="ci"></a>`
 
       if (status == 'off') {
         const ticket_url = 'https://jira.greenpeace.org/browse/' + item[1];
-        html += `<a href="${ticket_url}" target="_blank" class="link"><img src="static/tag.svg" alt="ticket"></a>`;
+        html += `<a href="${ticket_url}" target="_blank" class="link"><img src="static/tag.svg" alt="ticket" title="ticket"></a>`;
       }
 
       html += `</div>`;
